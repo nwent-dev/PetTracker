2fdceb1ac9bf7e1bf0d3f011248d44a1259bdba7
alk <antondobrfa@gmail.com>
2025-03-27 23:57:34 +0300

    Initial Commit

---
+++ PetTracker/PetTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetTrackerApp.swift
+//  PetTracker
+//
+//  Created by Александр Калашников on 27/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PetTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
